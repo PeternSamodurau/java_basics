3d4a031d1e602a23f4f9c4cd8a7eab7ce7b4700d
Peter Samodurov <spvrent@mail.ru>
2022-09-27 17:47:35 +0300

    6.8 practical task 2 ver.2

--- ObjectsAndClasses_StaticFinal/srs/CentralProcessor.java
+++
@@ -1,8 +0,0 @@
-package srs;
-
-enum CentralProcessor {
-    PROCESSOR_CLOCK_RATE,               // Тактовая частота процессора
-    PROCESSOR_AMOUNT_CORE,              //  Количество ядер процессора
-    PROCESSOR_MANUFACTURER,             //  Производитель процессора
-    PROCESSOR_WEIGHT;                   //  Вес процессора
-}
